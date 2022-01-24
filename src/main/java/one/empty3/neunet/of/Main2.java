eb5380cd6ba59069dbc858a951ad31892f398a8d
dathewolf <manuel.dahmen@gmx.com>
2022-01-24 18:57:15 +0100

    Make dataset

--- src/main/java/one/empty3/neunet/of/Main2.java
+++ src/main/java/one/empty3/neunet/of/Main2.java
@@ -6,12 +6,21 @@ import atlasgen.CsvReader;
 import one.empty3.neunet.*;
 
 import javax.swing.*;
+import javax.swing.JFileChooser;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
 
 public class Main2 {
     private static JRootPane rootPane;
+    private static JFrame frame;
     private String csv;
-    private JButton button1;
+    private JButton parcourirButton;
     private JTextField textField1;
     private JTextField textField2;
     private JTextField textField3;
@@ -22,11 +31,84 @@ public class Main2 {
     private JButton reloadButton;
     private JButton saveButton;
     private JPanel imagePanel;
+    private JButton suivantButton;
+    private JButton précédentButton;
+    private JList listImages;
+    private int startIndex = 0;
+    private int pageSize = 20;
+    CsvLine csvLineSelected;
+
+    private List<String[]> csvValues = new ArrayList<>();
+    private CsvReader reader;
+
+    public Main2() {
+        parcourirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser(new File(csv));
+                chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                chooser.showDialog(frame, "Select");
+                if(chooser.getSelectedFile()!=null) {
+                    csv = chooser.getCurrentDirectory().getAbsolutePath();
+                    populate();
+                }
+                
+            }
+        });
+        listImages.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                ListModel objects = listImages.getModel();
+                Object elementAt = objects.getElementAt(listImages.getSelectedIndex());
+                String idStr = (String)elementAt;
+                reader = new CsvReader(new File(csv), "\t", "\n", false);
+                reader.setAction(new Action() {
+                    String id = idStr;
+
+                    @Override
+                    public void init() {
+
+                    }
+
+                    @Override
+                    public void processLine(CsvLine csvLine) {
+                        if(csvLine.getValue()[0].equals(id)) {
+                            csvLineSelected = csvLine;
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+
+    private void populate() {
+        Vector listData = new Vector();
+        reader = new CsvReader(new File(csv), "\t", "\n", false);
+            reader.setAction(new Action() {
+                int i=0;
+                @Override
+                public void init() {
+
+                }
+
+                @Override
+                public void processLine(CsvLine csvLine) {
+                    i++;
+                    if(i>=startIndex&&i<startIndex+pageSize){
+                        String s = csvLine.getValue()[0];
+                        listData.add(s);
+                    }
+                }
+            });
+        listImages.setListData(listData);
+    }
 
     public static void main(String [] args) {
 
 
-        JFrame frame = new JFrame("Test");
+        frame = new JFrame("Test");
         rootPane = new Main().getRootPane();
         frame.setContentPane(rootPane);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -36,24 +118,4 @@ public class Main2 {
 
     }
 
-    public void loadData() {
-        File directory = new File(csv);
-        CsvReader reader = new CsvReader(directory, "\t", "\n", false);
-        if (directory.exists() && directory.isFile()) {
-            reader.setAction(new Action() {
-                @Override
-                public void init() {
-
-                }
-
-                @Override
-                public void processLine(CsvLine csvLine) {
-                    String[] value = csvLine.getValue();
-
-                }
-            });
-            reader.process();
-
-        }
-    }
 }
